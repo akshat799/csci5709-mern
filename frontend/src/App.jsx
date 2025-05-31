1825769b5e4af6fbf9e15f8d247a5d4473831e64
Akshat Gulati <59341675+akshat799@users.noreply.github.com>
2025-05-31 00:06:35 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Home from './pages/Home/Home';
+import Home from './pages/Home/home';
 import Product from './pages/Product/Product';
 import Contact from './pages/Contact/Contact';
 
